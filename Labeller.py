613c7973c13aca772e2b15e4d54a28b012eae08d
normanqw <normanqw@umich.edu>
2021-10-28 13:52:50 -0400

    add final words

--- Labeller.py
+++ Labeller.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from pathlib import Path
-import glob, os
+import os
 import csv
 from PIL import Image, ImageTk
 
@@ -12,7 +12,7 @@ class Labeller(tk.Frame):
     def __init__(self, master, dir_path_str: str, data_type: str):
         super().__init__(master)
         self.master = master
-                
+
         """Initialise labeller variables"""
         # input/output directory, which contains
         try:
@@ -25,13 +25,15 @@ class Labeller(tk.Frame):
         self.add_all_files()
         self.img = None
         self.panel = None
+        self.yes = None
+        self.no = None
         self.img_tk = None
 
         # check if such a file exists
         self.config_file_path = self.dir / "config.txt"
         self.csv_output_path = self.dir / "labels.csv"
-        
-        self.last_labelled_idx = -1
+
+        self.current_image_idx = 0
         if not self.config_file_path.exists():
             print("New work session")
             # initialise the config file
@@ -41,21 +43,35 @@ class Labeller(tk.Frame):
             self.create_csv_file()
         else:
             print("Resuming from a checkpoint")
-            self.last_labelled_idx = self.read_config_file()
+            self.current_image_idx = self.read_config_file()
 
         self.opened_csv = None
-        self.current_image_idx = self.last_labelled_idx + 1
+        self.current_image_idx += 1
         # open csv file, append the csv file
         self.render_initial()
-    
+
     def save_check_point(self):
         """Save the labelled data back into """
         with open(self.config_file_path, 'w') as self.config_file:
-            self.config_file.write(str(self.last_labelled_idx))
+            self.config_file.write(str(self.current_image_idx))
 
     def get_current_image_path(self):
         """Return the path to the current image that we are labelling."""
+        if self.current_image_idx >= len(self.files):
+            self.render_finished()
+            return str(self.dir / self.files[len(self.files) - 1])
+
         return str(self.dir / self.files[self.current_image_idx])
+
+    def render_finished(self):
+        print("Finished Labelling")
+        finished = tk.Label(self.master, text='Finished Labelling')
+        if self.yes is not None:
+            self.yes.state = "disabled"
+        if self.no is not None:
+            self.no.state = "disabled"
+        finished.pack()
+        exit()
 
     def render_next(self):
         """Render the next image."""
@@ -65,7 +81,6 @@ class Labeller(tk.Frame):
         if self.panel is not None:
             self.panel.configure(image=self.img_tk)
             self.panel.image = self.img_tk
-
 
     def render_initial(self):
         """Render the image and the options."""
@@ -77,7 +92,8 @@ class Labeller(tk.Frame):
 
         # render the options: yes or no; and bind to keyboard events
         # reference: https://codereview.stackexchange.com/questions/191477/binding-a-keyboard-key-to-a-tkinter-button
-        self.yes = ttk.Button(self.master, text="YES", command=self.yes_callback)
+        self.yes = ttk.Button(self.master, text="YES",
+                              command=self.yes_callback)
         self.master.bind('y', lambda event: self.yes_callback())
 
         self.no = ttk.Button(self.master, text="NO", command=self.no_callback)
@@ -87,17 +103,22 @@ class Labeller(tk.Frame):
         self.no.pack()
 
     def open_csv_output(self, func):
-        with open(self.csv_output_path, 'w') as self.opened_csv:
+        with open(self.csv_output_path, 'a') as self.opened_csv:
             self.csv_writer = csv.writer(self.opened_csv)
             func()
 
     def write_to_csv(self, filename, value):
-        self.open_csv_output(lambda :
-                             self.csv_writer.
-                             writerow([filename, value]))
-        self.last_labelled_idx += 1
-        self.current_image_idx = self.last_labelled_idx + 1
-        self.render_next()
+
+        def write():
+            self.csv_writer.writerow([filename, value])
+
+        self.open_csv_output(write)
+        self.save_check_point()
+        self.current_image_idx += 1
+        if self.current_image_idx != len(self.files):
+            self.render_next()
+        else:
+            self.render_finished()
 
     def yes_callback(self):
         """Write a yes to the output file."""
@@ -109,24 +130,23 @@ class Labeller(tk.Frame):
 
     def add_all_files(self):
         """Index all the files.
-        
+
         Assume glob.glob iterations' order doesn't change across runs...
         """
         for file in os.listdir(self.dir):
             if file.endswith(f".{self.data_type}"):
-            # append all the files
+                # append all the files
                 self.files.append(file)
 
     def init_config(self):
         """Touch a configuration file."""
         with open(self.config_file_path, 'w') as self.config_file:
-            self.config_file.write("-1") # 
+            self.config_file.write("-1")  #
 
     def create_csv_file(self):
         """Create a csv file for output"""
         with open(self.csv_output_path, 'w') as _:
             pass
-        
 
     def read_config_file(self):
         """Read the configuration file and return . Should only contain 1 number."""
